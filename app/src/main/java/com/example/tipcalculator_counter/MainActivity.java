63947cde90f2a8f0ce36a369abfbfdd4219a02c7
cooperkm <cooperkm@g.cofc.edu>
2019-02-12 20:04:18 -0500

    Finished layout for number of people and bill amount

--- app/src/main/java/com/example/tipcalculator_counter/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_counter/MainActivity.java
@@ -5,11 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* TODO
-    *  # of people button
-    *
-    *
-    * */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
